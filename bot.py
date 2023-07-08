ca6af5f0f9268f35317adfd51b461abcc363721b
KamenevArtem <kamenevart94@gmail.com>
2023-07-08 23:52:09 +0300

    initial commit

--- bot.py
+++ bot.py
@@ -1,14 +1,59 @@
+import logging
 import os
 
 import telegram
 
 from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Filters
+from telegram.ext import CommandHandler
+from telegram.ext import CallbackContext
+from telegram.ext import MessageHandler
+from telegram.ext import Updater
+
+
+def start(update: Update, context=CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Hi'
+        )
+
+
+def echo(update: Update, context=CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=update.message.text
+    )
 
 
 def main():
     load_dotenv()
-    bot_token = os.environ('BOT_TOKEN')
-    bot = telegram.Bot(token=bot_token)
+    bot_token = os.environ['BOT_TOKEN']
+    chat_id = os.environ['CHAT_ID']
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    updater = Updater(
+        token=bot_token,
+        use_context=True
+        )
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler(
+        'start',
+        start
+        )
+    echo_handler = MessageHandler(
+        Filters.text & (~Filters.command),
+        echo
+    )
+    dispatcher.add_handler(
+        start_handler
+        )
+    dispatcher.add_handler(
+        echo_handler
+    )
+    updater.start_polling()
 
 
 if __name__ == '__main__':
