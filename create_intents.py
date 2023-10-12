5230120eee3f90be0eac965df14b81765f843957
KamenevArtem <kamenevart94@gmail.com>
2023-10-12 03:23:19 +0300

    add DialogFlow learning script

---
+++ create_intents.py
@@ -0,0 +1,61 @@
+import json
+import os
+
+from dotenv import load_dotenv
+from google.cloud import dialogflow
+
+
+def list_intents(project_id):
+    intents_client = dialogflow.IntentsClient()
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    intents = intents_client.list_intents(request={"parent": parent})
+    for intent in intents:
+        print("=" * 20)
+        print("Intent name: {}".format(intent.name))
+        print("Intent display_name: {}".format(intent.display_name))
+        print("Action: {}\n".format(intent.action))
+        print("Root followup intent: {}".format(intent.root_followup_intent_name))
+        print("Parent followup intent: {}\n".format(intent.parent_followup_intent_name))
+        print("Input contexts:")
+        for input_context_name in intent.input_context_names:
+            print("\tName: {}".format(input_context_name))
+        print("Output contexts:")
+        for output_context in intent.output_contexts:
+            print("\tName: {}".format(output_context.name))
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+    text = dialogflow.Intent.Message.Text(text=[message_texts])
+    message = dialogflow.Intent.Message(text=text)
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+    print("Intent created: {}".format(response))
+
+
+def main():
+    load_dotenv()
+    project_id = os.environ["PROJECT_ID"]
+    with open("situations.json", "r") as my_file:
+        situations_json = my_file.read()
+        situations = json.loads(situations_json)
+        for situation in situations:
+            situation_content = situations[situation]
+            questions = situation_content['questions']
+            answer = situation_content['answer']
+            create_intent(project_id, situation, questions, answer)
+    list_intents(project_id)
+
+
+if __name__ == "__main__":
+    main()
